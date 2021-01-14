972df0bf207684ca5638fcb9241115b82b5028b5
mykhailo_hunko <mykhailo.hunko@epam.com>
2021-01-14 23:41:35 +0200

    Every time create new connection

--- arbiter/rabbit_connector.py
+++ arbiter/rabbit_connector.py
@@ -1,35 +1,36 @@
 import pika
 import logging
 
-connection = None
+#connection = None
 
 
 def _get_connection(config):
-    global connection
-    logging.info(f"!!!!!!!!!!!!Connection: {connection}")
-    if not connection:
-        _connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=config.host,
-                port=config.port,
-                virtual_host=config.vhost,
-                credentials=pika.PlainCredentials(
-                    config.user,
-                    config.password
-                )
+    #global connection
+    #logging.info(f"!!!!!!!!!!!!Connection: {connection}")
+    #if not connection:
+    _connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=config.host,
+            port=config.port,
+            virtual_host=config.vhost,
+            credentials=pika.PlainCredentials(
+                config.user,
+                config.password
             )
         )
-        channel = _connection.channel()
-        channel.exchange_declare(
-            exchange=config.all,
-            exchange_type="fanout", durable=True
-        )
-        connection = channel
-    try:
-        connection._process_data_events(time_limit=0)
-    except (pika.exceptions.StreamLostError, pika.exceptions.ChannelClosedByBroker):
-        logging.info("!!!!!!!!!!!!!!!!!!!!!Got exception in _get_connection method")
-        connection = None
-        return _get_connection(config)
+    )
+    channel = _connection.channel()
+    channel.exchange_declare(
+        exchange=config.all,
+        exchange_type="fanout", durable=True
+    )
+        # except pika.exceptions.StreamLostError:
+    connection = channel
+    # try:
+    #     connection._process_data_events(time_limit=0)
+    # except (pika.exceptions.StreamLostError, pika.exceptions.ChannelClosedByBroker):
+    #     logging.info("!!!!!!!!!!!!!!!!!!!!!Got exception in _het_connection method")
+    #     connection = None
+    #     return _get_connection(config)
 
     return connection
