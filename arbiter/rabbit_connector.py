2bea5075cac238917cbf7ae89fc863598a0e931c
mykhailo_hunko <mykhailo.hunko@epam.com>
2021-01-14 19:56:37 +0200

    Fix _get_connection method

--- arbiter/rabbit_connector.py
+++ arbiter/rabbit_connector.py
@@ -1,37 +1,37 @@
 import pika
 import logging
 
-connection = None
+#connection = None
 
 
 def _get_connection(config):
-    global connection
-    logging.info(f"!!!!!!!!!!!!Connection: {connection}")
-    if not connection:
-        _connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=config.host,
-                port=config.port,
-                virtual_host=config.vhost,
-                credentials=pika.PlainCredentials(
-                    config.user,
-                    config.password
-                )
+    #global connection
+    #logging.info(f"!!!!!!!!!!!!Connection: {connection}")
+    #if not connection:
+    _connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=config.host,
+            port=config.port,
+            virtual_host=config.vhost,
+            credentials=pika.PlainCredentials(
+                config.user,
+                config.password
             )
         )
-        channel = _connection.channel()
-        channel.queue_declare(
-            queue=config.light, durable=True
-        )
-        channel.queue_declare(
-            queue=config.heavy, durable=True
-        )
-        channel.exchange_declare(
-            exchange=config.all,
-            exchange_type="fanout", durable=True
-        )
+    )
+    channel = _connection.channel()
+    channel.queue_declare(
+        queue=config.light, durable=True
+    )
+    channel.queue_declare(
+        queue=config.heavy, durable=True
+    )
+    channel.exchange_declare(
+        exchange=config.all,
+        exchange_type="fanout", durable=True
+    )
         # except pika.exceptions.StreamLostError:
-        connection = channel
+    connection = channel
     # try:
     #     connection._process_data_events(time_limit=0)
     # except (pika.exceptions.StreamLostError, pika.exceptions.ChannelClosedByBroker):
